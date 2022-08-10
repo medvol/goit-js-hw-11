fc342dd99c2e73cd9acdb8ca04ce0563f15f34de
Oleh Medvid <olmedvid@gmail.com>
2022-08-11 00:08:13 +0300

    add: simplebox library and Notify library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 import { getPictures } from "./js/fetch-picture";
@@ -24,10 +26,23 @@ async function handleSubmit(event) {
     refs.galleryWrapper.innerHTML = '';
     const searchValue = refs.input.value;
     const value = await getPictures(searchValue);
-    if(value.data.hits.length < 0) {Notify.info('Cogito ergo sum')};
-    console.log(value.data.hits.length)
+    Notify.success(`Hooray! We found ${value.data.totalHits} images.`);
+ 
+     console.log(value)
+    console.log('1')
+    
+    if (value.data.hits.length > 0) {
+        console.log('center')
+        // return  Notify.info('Cogito ergo sum')
+    };
+    console.log('2')
+   
     const markup = createMarkup(value);
-    refs.galleryWrapper.insertAdjacentHTML('beforeend', markup)
+    refs.galleryWrapper.insertAdjacentHTML('beforeend', markup);
+    const lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionDelay: 250,
+});
    
 
 }
--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -1,25 +1,31 @@
 export function createMarkup(values) {
 
     return values.data.hits.reduce((acc, value) => {
-        const { webformatURL, tags, likes, views, comments, downloads } = value;
-        return acc + `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>${downloads}
-    </p>
-  </div>
-</div>`
+        const { webformatURL,largeImageURL, tags, likes, views, comments, downloads } = value;
+      return acc + `<div class="photo-card">
+       <div class="image-wrapper">
+          <a href="${largeImageURL}">
+           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          </a>
+        </div>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </div>`
     }, '')
     
 }
+
+
 
