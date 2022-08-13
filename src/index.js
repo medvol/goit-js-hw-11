f900875d0c2a4fb7357386b2208eb63dd49ae23d
Oleh Medvid <olmedvid@gmail.com>
2022-08-13 22:05:50 +0300

    add: throttle and styles to html

--- src/index.js
+++ src/index.js
@@ -2,50 +2,164 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-
+import InfiniteScroll from 'infinite-scroll';
+import throttle from 'lodash.throttle';
 import { getPictures } from "./js/fetch-picture";
-import { createMarkup } from "./js/create-markup";
-
-
-
+import { createGalleryMarkup } from "./js/create-markup";
 
 const refs = {
     form: document.querySelector('#search-form'),
     input: document.querySelector('.search-input'),
     searchBtn: document.querySelector('.search-btn'),
     loadMoreBtn: document.querySelector('.load-more'),
-    galleryWrapper: document.querySelector('.gallery')
+    galleryWrapper: document.querySelector('.gallery'),
+    infiniteScroll: document.querySelector('.pagination__next')
 
 }
 
+const urlParams = {
+    BASE_URL: 'https://pixabay.com/api',
+    KEY:  '17376022-5b12512663328c07708b99c13',
+    perPage: '40',
+    page: 1,
+   
+}
+
+let searchItem = ''
+let page = 1;
+
 refs.form.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', handleSubmit)
 
 async function handleSubmit(event) {
     event.preventDefault();
-    if(event.type === 'submit') refs.galleryWrapper.innerHTML = '';
+    const { BASE_URL, KEY, perPage, page} = urlParams;
+    updateUI(event);
+    searchItem = refs.input.value;
+
     
-    const searchValue = refs.input.value;
-    if (searchValue === '') return Notify.failure('Please, enter something');
-    const value = await getPictures(searchValue);
     
-    if (value.hits.length === 0) return Notify.info("Sorry, there are no images matching your search query. Please try again.");
-         
-   
-    Notify.success(`Hooray! We found ${value.totalHits} images.`);
- 
-    //  console.log(value)
-    // console.log('1')
+    const valueQuery = await searchQuery(searchItem);
+    refs.infiniteScroll.href=`${BASE_URL}/?key=${KEY}&q=${searchItem}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page+1}.html`
     
-    // 
-    // console.log('2')
-   
-    const markup = createMarkup(value);
+    const markup = createGalleryMarkup(valueQuery);
     refs.galleryWrapper.insertAdjacentHTML('beforeend', markup);
-    const lightbox = new SimpleLightbox('.gallery a', { 
+    const lightbox = new SimpleLightbox('.gallery a', {
     
-    captionDelay: 250,
-});
+        captionDelay: 250,
+    });
+   
+}
+
+function updateUI(event) {
+    if (event.type === 'submit') {
+    refs.galleryWrapper.innerHTML = '';
+    page = 1;
+};
+    
+}
+
+async function searchQuery(value) {
+    if (value === '') return Notify.failure('Please, enter something'); 
+    
+    const searchQuery = await getPictures(value, page); 
+    
+    if (searchQuery.hits.length === 0) return Notify.info("Sorry, there are no images matching your search query. Please try again.");    
+    
+    Notify.success(`Hooray! We found ${searchQuery.totalHits} images.`);
+    page += 1;
+    return searchQuery;
+    
+}
+
+
+// function createURL(urlParams, searchTerm) {
+//     const{BASE_URL, KEY, perPage,page} =urlParams
+//     const url = `${BASE_URL}/?key=${KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page+1}`
+//     return url
+// }
+
+// const url = createURL(urlParams, "cat")
+// console.log(url)
+
+// let nextPages = [
+//   'index',
+//   'options',
+//   'api',
+//   'events',
+//   'extras',
+//   'license',
+// ];
+
+
+
+addEventListener('scroll', throttle(onScroll, 500) )
+
+// let nextURL;
+
+// function updateNextURL( doc ) {
+//   nextURL = doc.querySelector('.pagination__next').getAttribute('href');
+// }
+// // get initial nextURL
+// console.log(updateNextURL( document ));
+
+function onScroll(event) {
+    
+    let infScroll = new InfiniteScroll( refs.galleryWrapper, {
+        path:
+            function () {
+    return (`${urlParams.BASE_URL}/?key=${urlParams.KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=${urlParams.perPage}&page=${this.pageIndex+1}`)},
+    responseBody: 'json',
+  append: '.photo-card',
+  status: '.scroller-status',
+        hideNav: '.pagination',
+  history: false,
+    });
    
 
+infScroll.on('load', function (body) {
+    console.log(body)
+  // compile body data into HTML
+    // const parseJson = JSON.parse(body)
+    const itemsHTML = createGalleryMarkup(body);
+     refs.galleryWrapper.insertAdjacentHTML('beforeend', itemsHTML);
+  // convert HTML string into elements
+//   proxyElem.innerHTML = itemsHTML;
+  // append item elements
+//   container.append( ...proxyElem.children );
+});
+   
+    console.log(infScroll.pageIndex)
+    
+//     infScroll.loadNextPage().then( function( loaded ) {
+//   // next page has been loaded
+//         console.log(loaded)
+//   let { response, body, items } = loaded;
+//   console.log( response.path );
+//   console.log( body );
+//         console.log(items);
+//          const markup = createGalleryMarkup(body);
+//     refs.galleryWrapper.insertAdjacentHTML('beforeend', markup);
+//     });
+    
+    infScroll.loadNextPage()
+    // let path = infScroll.getPath()
+    // infScroll.options.path = path
+    // console.log(path)
+ 
+//     infScroll.on('load', function (event, data) {
+//         const markup = createGalleryMarkup(body);
+//     refs.galleryWrapper.insertAdjacentHTML('beforeend', markup);
+//   console.log(`Loaded: ${path}`,
+//     `Status: ${response.status}`,
+//     `Current page: ${infScroll.pageIndex}`,
+//     `${infScroll.loadCount} pages loaded`
+    
+//         );
+//         console.log(data)
+// });
+    
+
+    
 }
+
--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -1,26 +1,41 @@
-export function createMarkup(values) {
+export function createGalleryMarkup(values) {
 
     return values.hits.reduce((acc, value) => {
         const { webformatURL,largeImageURL, tags, likes, views, comments, downloads } = value;
-      return acc + `<div class="photo-card">
+      return acc + `<div class="photo-card col ">
+      <div class="card shadow-sm image-overflow">
        <div class="image-wrapper">
           <a href="${largeImageURL}">
            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
           </a>
         </div>
         <div class="info">
-          <p class="info-item">
-            <b>Likes</b>${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>${downloads}
-          </p>
+        <div class="icon-wrapper">
+        <i class="bi bi-heart-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg></i>
+          <p class="info-item">${likes}</p>
+          </div>
+          <div class="icon-wrapper">
+          <i class="bi bi-eye-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+</svg></i>
+          <p class="info-item">${views}</p>
+          </div>
+          <div class="icon-wrapper">
+        <i class="bi bi-chat-left-text-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+</svg></i>
+          <p class="info-item">${comments}</p>
+          </div>
+          <div class="icon-wrapper">
+          <i class="bi bi-cloud-arrow-down-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
+  <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+</svg></i>
+          <p class="info-item">${downloads}</p>
+          </div>
+        </div>
         </div>
       </div>`
     }, '')
